427e6d6c6efbd5451b8d1197fdc85ba76e9730ab
BautiDeAngelis <juanbautistadeangelis@gmail.com>
2023-11-17 08:12:51 -0300

    Showing spinner when fetching genres.

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,5 +1,3 @@
-import { useEffect, useState } from 'react';
-import apiClient from '../services/api-client';
 import { SimpleGrid, Text } from '@chakra-ui/react';
 import useGames from '../hooks/useGames';
 import GameCard from './GameCard';
