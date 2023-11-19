c72fe63fd56c7d67762d2e88bca8572a3d3190e1
bautidea <juanbautistadeangelis@gmail.com>
2023-11-19 10:57:36 -0300

    Handling games with no img with a placeholder.

--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -1,9 +1,14 @@
+import noImage from '../assets/Image Placeholder/no-image-placeholder-6f3882e0.webp';
+
 // In this module we are modifying the url of the request we use to obtain the
 // images, because they are way too big and that can be a problem when loading the
 // app, so we are going to reduce the size of the img.
 
 const getCroppedImageUrl = (url: string) => {
-  if (!url) return '';
+  // If the url that we are passing doesnt have an img, we are going to return
+  // a Image Placeholder.
+  if (!url) return noImage;
+
   // Here we are obtaining the index of where the word 'media/' is located
   // the size of the image can be adjusted, for example here
   // https://media.rawg.io/media/games/942/9424d6bb763dc38d9378b488603c87fa.jpg
